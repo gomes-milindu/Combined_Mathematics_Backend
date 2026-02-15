24278027637028dde557e7506557eb5e1d69f7b5
gomes-milindu <gomesmilindu@gmail.com>
2026-02-15 16:16:11 +0530

    upload dashboard controller

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -19,7 +19,7 @@ export async function getDashboardStats(req, res) {
     ]);
 
     const totalIncome = totalIncomeData[0]?.total || 0;
-    console.log("Total Income for this month:", totalIncome);
+    
 
 
 
