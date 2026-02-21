22612b27b84e6c59c3fcb167ae75ff10a5e3fa9e
Milindu Gomes <gomesmilindu@gmail.com>
2026-02-21 11:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const allowedOrigins = new Set(
   [
     "http://localhost:5173",
     "http://localhost:3000",
+    "https://combined-mathematics-frontend.vercel.app",
     process.env.FRONTEND_URL,
   ].filter(Boolean)
 );
